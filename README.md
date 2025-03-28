814151f63174ecd3f26c26fd9da4ec627fd0f4ca
D-Man2798 <donovanclofer98@gmail.com>
2025-03-28 14:20:45 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SmartBrain App
+
+SmartBrain is a web application designed to detect faces in images. Users can upload image URLs, and the app will highlight any detected faces using advanced image recognition technology. Built with a modern tech stack, SmartBrain provides a seamless and interactive user experience.
+
+## Features
+- Face detection in images using AI.
+- User authentication and profile management.
+- Responsive and user-friendly interface.
+
+## Technologies Used
+- Frontend: React.js
+- Backend: Node.js, Express.js
+- Database: PostgreSQL
+- Machine Learning API: Clarifai API
+
+## Getting Started
+Clone the repository, install dependencies, and follow the setup instructions to run the app locally.
---
+++ src/App.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import './App.css';
+const MODEL_ID = 'face-detection'
+
+const returnClarifaiRequestOptions = (imageUrl) =>{
+  const PAT = '8af8b7c426d94392a40b428ecf720e24';
+  const USER_ID = 'dman2798';
+  const APP_ID = 'Smart-Brain';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn : false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  calculateFaceLocation = (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputImage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+   return{
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+   }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(this.state.input))
+    .then(response => response.json())
+    .then(response => {
+      if(response){
+        fetch('http://localhost:3000/image',{
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+      }
+      return response;
+    })
+    .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+    .catch(error => console.log('error', error));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+
+    this.setState({route: route});
+  }
+
+  render(){
+   const {isSignedIn, imageUrl, route, box } = this.state;
+   return (
+      <div className="App">
+        <ParticlesBg color="#FFFFFF" type="cobweb" bg={true} />
+        <Navigation isSignedin={isSignedIn} onRouteChange={this.onRouteChange}/>
+
+        {route === 'home'
+         ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route === 'signin' 
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+ 
+        }
+     </div>
+    );
+  }
+}
+
+export default App;
